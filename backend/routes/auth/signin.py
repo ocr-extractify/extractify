369d3345350c42dd3bf9e8abdca35f4eb47c2171
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-01-06 22:32:18 -0300

    wip: developing signin route

--- backend/routes/auth/signin.py
+++ backend/routes/auth/signin.py
@@ -5,7 +5,17 @@ from routes.auth import auth_router
 @auth_router.post(
     "/signin/",
     status_code=status.HTTP_204_NO_CONTENT,
-    response_model=None,
 )
-async def signin():
-    pass
+async def signin() -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
