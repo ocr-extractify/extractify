e7975fdcf0807a5eed7e6039c9b03ebd798b79de
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-01-22 22:25:30 -0300

    feat: render <Outlet />

--- frontend/src/pages/Base/index.tsx
+++ frontend/src/pages/Base/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@/components/ui/sidebar"
 import { useAuth } from "@/providers/AuthProvider"
 import { Navigate } from "react-router-dom"
+import { Outlet } from "react-router"
 
 export default function Base() {
   const { isAuthenticated } = useAuth();
@@ -35,24 +36,19 @@ export default function Base() {
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
-                    Building Your Application
+                    File to text
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                  <BreadcrumbPage>Upload</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <Outlet />
         </div>
       </SidebarInset>
     </SidebarProvider>
