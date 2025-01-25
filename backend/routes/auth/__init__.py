1efd585e2397ed735ab3e7e72e47ab71d7f6bcf7
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-01-25 12:15:53 -0300

    refactor: update import statements for authentication routes

--- backend/routes/auth/__init__.py
+++ backend/routes/auth/__init__.py
@@ -6,6 +6,6 @@ auth_router = APIRouter(
 )
 
 
-from routes.auth.signin import *
-from routes.auth.signup import *
-from routes.auth.get_current_user import *
+from routes.auth.signin_route import *
+from routes.auth.signup_route import *
+from routes.auth.get_current_user_route import *
