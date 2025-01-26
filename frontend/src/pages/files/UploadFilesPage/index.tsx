de22637279e6b6a3c2a3acd7e2809380caeb4a78
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-01-25 23:15:37 -0300

    feat: enhance UploadFilesPage with translation support and improved layout

--- frontend/src/pages/files/UploadFilesPage/index.tsx
+++ frontend/src/pages/files/UploadFilesPage/index.tsx
@@ -14,8 +14,11 @@ import { NO_FILE } from '@/constants/errorsMsgs';
 import { BsFillFileEarmarkArrowUpFill } from "react-icons/bs";
 import { Label } from '@/components/ui/label';
 import { FileUploader } from '@/components/ui/file-uploader';
+import { Separator } from '@radix-ui/react-separator';
+import { useTranslation } from 'react-i18next';
 
 function UploadFilesPage() {
+  const { t } = useTranslation();
   const [results, setResults] = useState<null | APIFile[]>(null);
   const [files, setFiles] = useState<File[] | []>([]);
   const addFile = useFilesStore((state: FileStoreState) => state.add);
@@ -56,31 +59,42 @@ function UploadFilesPage() {
   return (
     <div className="mx-auto w-full">
       {!results ? (
-        <form
-          className="w-full flex flex-col"
-          onSubmit={handleSubmit}
-        >
-          <Label className='mb-2'>Files</Label>
-          {/* <FileInput
-            id="files"
-            files={files}
-            setFiles={setFiles}
-            multiple
-            accept={VALID_MIMETYPES.join(',')}
-          /> */}
-          <FileUploader
-            value={files}
-            onValueChange={setFiles}
-            multiple
-          />
-          <Button
-            className="w-fit mt-2 mx-auto space-x-2 flex items-center"
-            isLoading={uploadFileMutation.isPending}
+        <div className="space-y-6">
+          <div>
+            <h3 className="text-lg font-medium">{t("UPLOAD")}</h3>
+            <p className="text-sm text-muted-foreground">
+              {t("UPLOAD_FILES_FORM_DESCRIPTION")}
+            </p>
+          </div>
+          
+          <Separator />
+
+          <form
+            className="w-full flex flex-col"
+            onSubmit={handleSubmit}
           >
-            <span className='uppercase font-medium'>{SUBMIT_BUTTON}</span>
-            <BsFillFileEarmarkArrowUpFill className='w-4 h-4'/>
-          </Button>
-        </form>
+            <Label className='mb-2'>Files</Label>
+            {/* <FileInput
+              id="files"
+              files={files}
+              setFiles={setFiles}
+              multiple
+              accept={VALID_MIMETYPES.join(',')}
+            /> */}
+            <FileUploader
+              value={files}
+              onValueChange={setFiles}
+              multiple
+            />
+            <Button
+              className="w-fit mt-2 mx-auto space-x-2 flex items-center"
+              isLoading={uploadFileMutation.isPending}
+            >
+              <span className='uppercase font-medium'>{SUBMIT_BUTTON}</span>
+              <BsFillFileEarmarkArrowUpFill className='w-4 h-4'/>
+            </Button>
+          </form>
+          </div>
       ) : (
         <div className="space-y-10 divide-y-2">
           {results.map((result) => (
