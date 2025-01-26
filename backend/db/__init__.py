0b55a360f086c799a0aea1a7ff231df22266ea05
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-01-26 17:20:17 -0300

    refactor: move database setup and session management to a new module

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,16 +1,2 @@
-from sqlalchemy import create_engine
-from sqlmodel import SQLModel, Session
-from config import config
+from db.setup_db import *
 from db.models import *
-from config import config
-
-engine = create_engine(str(config.SQLALCHEMY_DATABASE_URI))
-
-
-def setup_db():
-    SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
