9703056e36dfcc99782d286ac07d1d8b714913b6
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-02-05 19:45:26 -0300

    fix: enhance extract_data endpoint description and update language detection logic

--- backend/routes/files/extract_route.py
+++ backend/routes/files/extract_route.py
@@ -11,7 +11,7 @@ from utils.firebase import download
 
 @files_router.post(
     "/{id}/extract_data/",
-    description="Extract data from an existing file",
+    description="Extract data from an existing file. If the file is already extracted, it will return the existing data.",
     status_code=status.HTTP_201_CREATED,
 )
 async def extract_file_data(
@@ -43,8 +43,8 @@ async def extract_file_data(
     new_file_extraction = FileExtraction(
         name=db_file.name,
         text=analyzed_file["text"],
-        detected_languages=analyzed_file["detected_languages"],
-        extracted_data=analyzed_file["extracted_data"],
+        # all pdf have only one page. (bussiness rule)
+        detected_languages=analyzed_file["pages"][0]["detectedLanguages"],
         file_id=db_file.id,
         user_id=current_user.id,
     )
@@ -52,37 +52,3 @@ async def extract_file_data(
     session.commit()
     session.refresh(new_file_extraction)
     return new_file_extraction
-
-    # if request.client is None:
-    #     # TODO: Add detail error message.
-    #     raise HTTPException(status_code=400, detail="")
-
-    # check if there the quota per month is reached.
-    # if current_month_uploads_qty >= config.MONTHLY_UPLOADS_LIMIT:
-    #     raise HTTPException(status_code=400, detail=MONTHLY_LIMIT_REACHED)
-
-    # check if user day quota is reached
-    # is_client_restricted = request.client.host not in config.UNRESTRICTED_IPS
-    # if is_client_restricted:
-    #     start_of_day = datetime(now.year, now.month, now.day)
-    #     client_daily_uploads_qty = await files_collection.count_documents(
-    #         {"client_ip": request.client.host, "created_at": {"$gte": start_of_day}}
-    #     )
-    #     if client_daily_uploads_qty >= config.DAILY_UPLOADS_BY_IP_LIMIT:
-    #         raise HTTPException(status_code=400, detail=CLIENT_DAY_LIMIT_REACHED)
-
-    # analyzed_file = await analyze_file(file, request)
-    # with open("sample_analysis.json", "w", encoding="utf-8") as json_file:
-    #     json.dump(analyzed_file, json_file, indent=4, ensure_ascii=False)
-
-    #  analyzed_file = await clean_document_ai_analysis(analyzed_file)
-
-    # file_model = FileModel(
-    #     name=file.filename or str(uuid.uuid4()),
-    #     analysis=analyzed_file,
-    #     client_ip=request.client.host,
-    # )
-    # file_dict = file_model.model_dump(by_alias=True, exclude={"id"})
-    # new_db_file = await files_collection.insert_one(file_dict)
-    # created_file = await files_collection.find_one({"_id": new_db_file.inserted_id})
-    # return created_file
