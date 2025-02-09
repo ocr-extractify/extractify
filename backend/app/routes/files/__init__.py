25bfb4d4d425f2f1f686630b3d9cca06a0054030
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-02-08 22:07:13 -0300

    refactor: improve folder organization

--- backend/app/routes/files/__init__.py
+++ backend/app/routes/files/__init__.py
@@ -5,9 +5,9 @@ files_router = APIRouter(
     tags=["files"],
 )
 
-from app.routes.files.get_file_route import *
-from app.routes.files.get_files_route import *
-from app.routes.files.create_file_route import *
-from app.routes.files.delete_file_route import *
-from app.routes.files.create_ocr_extraction_route import *
-from app.routes.files.get_ocr_extraction_route import *
+from app.routes.files.files.get_file_route import *
+from app.routes.files.files.get_files_route import *
+from app.routes.files.files.create_file_route import *
+from app.routes.files.files.delete_file_route import *
+from app.routes.files.ocr_extractions.create_ocr_extraction_route import *
+from app.routes.files.ocr_extractions.get_ocr_extraction_route import *
