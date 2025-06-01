8da2ff4da4c423766cde75cb1af91d1a43401dc2
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-06-01 12:32:46 -0300

    feat: add initial setup scripts for frontend, git, nginx, postgresql, supervisor, and uv

---
+++ vm/app/frontend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+main() {
+    PROJECT_NAME="frontend"
+    PROJECT_PATH="/srv/frontend"
+    PROJECT_REPO="https://github.com/ocr-extractify/extractify"
+
+    setup_filesystem() {
+        if [ -d "$PROJECT_PATH" ]; then
+            rm -rf $PROJECT_PATH
+            setup_filesystem
+        else
+            git clone "$PROJECT_REPO" "$PROJECT_PATH"
+            cd "$PROJECT_PATH/$PROJECT_NAME"
+            mv * ../
+        fi
+    }
+}
+
+main
---
+++ vm/tools/postgresql.sh
@@ -0,0 +1,5 @@
+# ref: https://www.postgresql.org/download/linux/debian/ 
+sudo apt update
+sudo apt install -y postgresql-common
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+sudo apt -y install postgresql
