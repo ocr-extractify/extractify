69ed148a68488a9034db81c2cd92515462c1a2e3
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-06-01 13:26:17 -0300

    chore: remove obsolete setup scripts for API, frontend, nginx, and related tools (store it in another repo)

--- vm/tools/postgresql.sh
+++
@@ -1,5 +0,0 @@
-# ref: https://www.postgresql.org/download/linux/debian/ 
-sudo apt update
-sudo apt install -y postgresql-common
-sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-sudo apt -y install postgresql
