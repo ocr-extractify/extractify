01b6b641db95c28eebd26bd0d8a6e1a6d8115aae
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-06-05 23:07:28 -0300

    fix: fix route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Navigate to="/upload" />,
+        element: <Navigate to="/files/upload" />,
       },
       {
         path: '/files/upload',
