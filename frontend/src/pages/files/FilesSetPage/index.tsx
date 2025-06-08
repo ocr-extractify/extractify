2b17bb74e71c8074442d0f15e7588affee92b49d
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-06-08 15:45:55 -0300

    feat: add build

--- frontend/src/pages/files/FilesSetPage/index.tsx
+++ frontend/src/pages/files/FilesSetPage/index.tsx
@@ -1,7 +1,12 @@
 import { httpClient } from '@/utils/axios';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/flat-tabs"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/flat-tabs';
 import ExtractionResult from './fragments/ExtractionResult';
 import ExtractionConfig from './fragments/ExtractionConfig';
 import { useTranslation } from 'react-i18next';
@@ -15,23 +20,25 @@ const FilesSetPage = () => {
     queryFn: () => httpClient.get(`/files/sets/${id}`),
   });
 
-  console.log("filesSet", filesSet);
+  console.log('filesSet', filesSet);
   return (
     <>
       <h1 className="text-2xl">{filesSet.data?.data?.name}</h1>
 
       <Tabs defaultValue="extraction_result">
-        <TabsList >
-          <TabsTrigger value="extraction_result">{t('EXTRACTION_RESULT')}</TabsTrigger>
-          <TabsTrigger value="extraction_config">{t('EXTRACTION_CONFIG')}</TabsTrigger>
+        <TabsList>
+          <TabsTrigger value="extraction_result">
+            {t('EXTRACTION_RESULT')}
+          </TabsTrigger>
+          {/* <TabsTrigger value="extraction_config">{t('EXTRACTION_CONFIG')}</TabsTrigger> */}
         </TabsList>
 
         <TabsContent value="extraction_result">
           <ExtractionResult />
         </TabsContent>
-        <TabsContent value="extraction_config">
+        {/* <TabsContent value="extraction_config">
           <ExtractionConfig />
-        </TabsContent>
+        </TabsContent> */}
       </Tabs>
     </>
   );
--- frontend/src/pages/files/UploadFilesPage/index.tsx
+++ frontend/src/pages/files/UploadFilesPage/index.tsx
@@ -8,23 +8,26 @@ import { FileUploader } from '@/components/ui/file-uploader';
 import { Separator } from '@radix-ui/react-separator';
 import { useTranslation } from 'react-i18next';
 import { useToast } from '@/hooks/use-toast';
-import { DataExtractionRegexField, defaultRegexFields } from '@/constants/dataExtractionRegexFields';
+import {
+  DataExtractionRegexField,
+  defaultRegexFields,
+} from '@/constants/dataExtractionRegexFields';
 import RegexForm from './fragments/RegexForm';
 
 function UploadFilesPage() {
   const { t } = useTranslation();
   const { toast } = useToast();
   const [files, setFiles] = useState<File[] | []>([]);
-  const [regexFields, setRegexFields] = useState<DataExtractionRegexField[]>(defaultRegexFields);
+  const [regexFields, setRegexFields] =
+    useState<DataExtractionRegexField[]>(defaultRegexFields);
   const uploadFileMutation = useMutation({
     mutationFn: async (file: File) => {
       const formData = new FormData();
       formData.append('file', file);
 
-      const api_file = await httpClient
-        .post('/files/', formData, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
+      const api_file = await httpClient.post('/files/', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
       return api_file.data.id;
     },
   });
@@ -36,16 +39,16 @@ function UploadFilesPage() {
           regex_fields: regexFields,
         },
       );
-      console.log("api_ocr_extraction", api_ocr_extraction);
-    }
+      console.log('api_ocr_extraction', api_ocr_extraction);
+    },
   });
   const createFileSetMutation = useMutation({
     mutationFn: async (file_ids: string[]) => {
       const api_file_set = await httpClient.post('/files/sets/', {
-        name: t("FILES_SET") + " " + new Date().toLocaleString(),
+        name: t('FILES_SET') + ' ' + new Date().toLocaleString(),
         file_ids: file_ids,
       });
-      console.log("api_file_set", api_file_set)
+      console.log('api_file_set', api_file_set);
     },
   });
 
@@ -61,7 +64,7 @@ function UploadFilesPage() {
     await Promise.all(
       filesArray.map(async (file: File) => {
         const file_id = await uploadFileMutation.mutateAsync(file);
-        console.log("file_id", file_id);
+        console.log('file_id', file_id);
         createFileOcrExtractionMutation.mutateAsync(file_id);
         filesIds.push(file_id);
       }),
@@ -70,42 +73,38 @@ function UploadFilesPage() {
     createFileSetMutation.mutateAsync(filesIds);
   }
 
-  console.log("regexFields", regexFields);
+  console.log('regexFields', regexFields);
   return (
     <div className="mx-auto w-full">
       <div className="space-y-6">
         <div>
-          <h3 className="text-lg font-medium">{t("UPLOAD")}</h3>
+          <h3 className="text-lg font-medium">{t('UPLOAD')}</h3>
           <p className="text-sm text-muted-foreground">
-            {t("UPLOAD_FILES_FORM_DESCRIPTION")}
+            {t('UPLOAD_FILES_FORM_DESCRIPTION')}
           </p>
         </div>
 
         <Separator />
 
-        <RegexForm
-          regexFields={regexFields}
-          setRegexFields={setRegexFields}
-        />
+        <RegexForm regexFields={regexFields} setRegexFields={setRegexFields} />
 
-        <form
-          className="w-full flex flex-col"
-          onSubmit={handleSubmit}
-        >
-          <Label className='mb-2'>Files</Label>
+        <form className="w-full flex flex-col" onSubmit={handleSubmit}>
+          <Label className="mb-2">Files</Label>
 
           {/** TODO: use user limits to set FileUploader constraints. */}
           <FileUploader
             value={files}
             onValueChange={setFiles}
             multiple
+            // TODO: in future, use user limits to set FileUploader constraints.
+            maxFileCount={20}
           />
 
           <Button
             className="w-fit mt-4 flex items-center"
             isLoading={uploadFileMutation.isPending}
           >
-            <span className='uppercase font-medium'>{t("UPLOAD")}</span>
+            <span className="uppercase font-medium">{t('UPLOAD')}</span>
           </Button>
         </form>
       </div>
